797ff2cf52c31b5dde812e4d98aac340b7bcccc7
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-02-22 00:52:07 -0300

    feat:Exercicios

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,10 @@
-# Seu código aqui
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
+
+
+
+def get_score(team_name,team):
+     
+    return 'A '+team_name+ 'ficou classificada em'+
