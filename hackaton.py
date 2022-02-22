9f4b3f52aa39f5ac1552efb515cc896e839024ec
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-02-22 01:17:10 -0300

    fix:Bug fix

--- hackaton.py
+++ hackaton.py
@@ -1,3 +1,6 @@
+from black import out
+
+
 hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
 hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
 hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
@@ -6,5 +9,8 @@ hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
 
 
 def get_score(team_name,team):
-     
-    return 'A '+team_name+ 'ficou classificada em'+
+    output=''
+    for i in range (len(team)):
+        if team[i]==team_name:
+            output='A '+team[i]+' ficou classificada em '+str(i+1)
+            return output
